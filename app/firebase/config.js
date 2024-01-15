d5ab31f19915bed4b318ab854173836c840a5921
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-01-15 12:12:53 +0800

    fix: revert

--- app/firebase/config.js
+++ app/firebase/config.js
@@ -2,16 +2,15 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import {getAuth} from 'firebase/auth';
 
-
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyB7Mmw3hdKRae2A0FhjES_yf0TENKNIyZI",
+    authDomain: "zppsu-auth.firebaseapp.com",
+    projectId: "zppsu-auth",
+    storageBucket: "zppsu-auth.appspot.com",
+    messagingSenderId: "668228889853",
+    appId: "1:668228889853:web:64b8e22454953c95a3a294",
+    measurementId: "G-4J0B5JP38Z"
+  };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
 
