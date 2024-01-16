4c02ee0cbcbb2be403a0b29905cb446baed99d45
Ralph Saladino <130468937+hplar05@users.noreply.github.com>
2024-01-16 21:48:48 +0800

    update: navbar

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="h-20 w-full border-b-2 flex items-center justify-between p-2">
+      <ul className="flex ml-6">
+        <li>LOGO</li>
+      </ul>
+      <ul className="flex">
+        <li className="p-2 cursor-pointer">
+          <Link href="/">Home</Link>
+        </li>
+        <li className="p-2 cursor-pointer">
+          <Link href="/">Profile</Link>
+        </li>
+        <li className="p-2 cursor-pointer">
+          <Link href="/">About</Link>
+        </li>
+        <li className="ml-10 mr-6 p-2 cursor-pointer">
+          <Link href="/sign-in">Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from './components/Navbar'
 
+
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -12,8 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
+      <body className={inter.className}>
       <Navbar/>
+        {children}
       </body>
     </html>
   )
